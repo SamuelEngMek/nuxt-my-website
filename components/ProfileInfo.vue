8bbfde31124c0aba9b97d49b8001b80f3551d184
SamuelEngMek <samuelrodrigues739@gmail.com>
2025-02-13 14:38:06 -0300

    Initial Project

---
+++ components/ProfileInfo.vue
@@ -0,0 +1,24 @@
+<template>
+    <figure class="p-6">
+        <img 
+        class="w-32 h-32 rounded-full mx-auto" 
+        :src="photo" 
+        alt="profile picture" 
+        width="400"
+        height="400"
+    />
+        <figure class="pt-6 text-center space-y-4">
+        <figcaption class="font-medium">
+            <div class="text-white text-xl">{{ title }}</div>
+            <div class="text-gray-100 font-light">{{subtitle}}</div>
+        </figcaption>
+        <blockquote>
+            <p class="text-white font-medium">{{headline}}</p>
+        </blockquote>
+        </figure>
+    </figure>
+</template>
+
+<script setup>
+    const { photo, title, subtitle, headline } = defineProps(['photo','title', 'subtitle', 'headline']);
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <ProfileInfo
+    photo="https://placehold.co/180x180"
+    title="Samuel Miranda"
+    subtitle="@samuel_2rm"
+    headline="Data & Analytics | AI | Software Engineer"
+  />
+
+  <main class="max-w-xs mx-auto">
+    <ButtonAction
+    link="dada"
+    icon="👨🏽‍💻"
+    text="linkedin"
+    />
+
+    <ButtonAction
+    link="dada"
+    icon="👨🏽‍💻"
+    text="linkedin"
+    />
+
+    <ButtonAction
+    link="dada"
+    icon="👨🏽‍💻"
+    text="linkedin"
+    />
+
+    <ButtonAction
+    link="dada"
+    icon="👨🏽‍💻"
+    text="linkedin"
+    />
+
+  </main>
+
+
+</template>
