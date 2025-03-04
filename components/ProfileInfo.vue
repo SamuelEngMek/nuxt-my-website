953945d94ae76476546081b6bfde0bf3b52131d0
SamuelEngMek <samuelrodrigues739@gmail.com>
2025-03-04 15:10:15 -0300

    "Ajuste no tamanho e estilo da imagem"

--- components/ProfileInfo.vue
+++ components/ProfileInfo.vue
@@ -1,24 +1,24 @@
 <template>
     <figure class="p-6">
-        <img 
-        class="w-32 h-32 rounded-full mx-auto" 
+      <img 
+        class="rounded-full mx-auto object-cover" 
         :src="photo" 
-        alt="profile picture" 
-        width="400"
-        height="400"
-    />
-        <figure class="pt-6 text-center space-y-4">
+        alt="profile picture"
+        style="width: 128px; height: 128px; object-fit: cover;" 
+      />
+      <figure class="pt-6 text-center space-y-4">
         <figcaption class="font-medium">
-            <div class="text-white text-xl">{{ title }}</div>
-            <div class="text-gray-100 font-light">{{subtitle}}</div>
+          <div class="text-white text-xl">{{ title }}</div>
+          <div class="text-gray-100 font-light">{{ subtitle }}</div>
         </figcaption>
         <blockquote>
-            <p class="text-white font-medium">{{headline}}</p>
+          <p class="text-white font-medium">{{ headline }}</p>
         </blockquote>
-        </figure>
+      </figure>
     </figure>
-</template>
-
-<script setup>
+  </template>
+  
+  <script setup>
     const { photo, title, subtitle, headline } = defineProps(['photo','title', 'subtitle', 'headline']);
-</script>
+  </script>
+  
--- components/StacksInfo.vue
+++ components/StacksInfo.vue
@@ -1,12 +1,11 @@
 <template>
-    <figure class="pt-32 text-center">
-        <h2 class="text-lg md:text-xl font-semibold text-white mb-2">Tecnologias & Frameworks</h2>
-
+    <figure class="pt-52 text-center">
+      <h2 class="text-lg md:text-xl font-semibold text-white mb-4">Tecnologias & Frameworks</h2>
+  
       <div class="mx-auto w-3/4 md:w-1/2 bg-gray-900 py-4 rounded-lg overflow-hidden">
         <div class="relative w-full overflow-hidden">
-          
           <div class="flex items-center animate-marquee">
-            <div class="flex items-center space-x-10" v-for="n in 2" :key="n">
+            <div class="flex items-center gap-8" v-for="n in 2" :key="n">
               <img 
                 v-for="(stack, index) in stacks" 
                 :key="index + '-loop-' + n" 
@@ -33,13 +32,23 @@
   <style>
   @keyframes marquee {
     0% { transform: translateX(0); }
-    100% { transform: translateX(-50%); } 
+    100% { transform: translateX(-50%); }
   }
   
   .animate-marquee {
     display: flex;
     animation: marquee 25s linear infinite;
-    width: max-content; 
+    width: max-content;
+  }
+  
+  h2 {
+    font-size: 1.25rem; /* Ajustando o tamanho da fonte para se adequar ao título */
+    margin-bottom: 1rem; /* Garantindo um bom espaçamento abaixo do título */
+  }
+  
+  img {
+    object-fit: contain; /* Garante que as imagens não distorçam */
+    height: 64px; /* Ajuste de altura para garantir consistência */
   }
   </style>
   
